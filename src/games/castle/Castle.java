297016a6572eab7a404ee8417556553bc4dafac2
Alec Dorrington <alec.dorrington@protonmail.com>
2019-04-20 13:29:48 +0800

    Launches a window.

--- src/games/castle/Castle.java
+++ src/games/castle/Castle.java
@@ -3,12 +3,20 @@ package games.castle;
 import java.util.Arrays;
 import java.util.List;
 
+import games.castle.render.Window;
+
 public class Castle {
 
 	private List<CastlePlayer> players;
 	
 	public Castle(CastlePlayer... players) {
 		this.players = Arrays.asList(players);
+		start();
+	}
+	
+	private void start() {
+		
+		Window window = new Window(1200, 900, "Castle Game v1.0");
 	}
 	
 	public interface CastlePlayer {}
---
+++ src/launch/Main.java
@@ -0,0 +1,10 @@
+package launch;
+
+import games.castle.Castle;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		new Castle();
+	}
+}
