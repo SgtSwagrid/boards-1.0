4217f3ad4eb1378efe064b18825b1626c67ca370
Alec Dorrington <alec.dorrington@protonmail.com>
2019-04-20 13:26:33 +0800

    Added .classpath to .gitignore

--- src/games/castle/Castle.java
+++ src/games/castle/Castle.java
@@ -1,5 +1,15 @@
 package games.castle;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Castle {
 
+	private List<CastlePlayer> players;
+	
+	public Castle(CastlePlayer... players) {
+		this.players = Arrays.asList(players);
+	}
+	
+	public interface CastlePlayer {}
 }
