23b7f45677e5653baa003b811a6d12e4a4df837b
Adrian <Adrian@Orion>
2019-04-21 13:34:39 +0800

    Formatting on dice class fixed

--- src/games/castle/util/Dice.java
+++ src/games/castle/util/Dice.java
@@ -13,8 +13,7 @@ public class Dice {
 	private int sides = 0;	
 	private Random rand;
 	
-	public Dice(int sides)
-	{
+	public Dice(int sides) {
 		this.sides = sides;
 		rand = new Random();
 	}
@@ -23,8 +22,7 @@ public class Dice {
 	 * Roll this dice once.
 	 * @return A single roll of this die
 	 */
-	public int roll()
-	{
+	public int roll() {
 		return rand.nextInt(sides) + 1;
 	}
 	
@@ -33,12 +31,10 @@ public class Dice {
 	 * @param times number of rolls
 	 * @return A list of rolls, in no particular order
 	 */
-	public int[] roll(int times)
-	{
+	public int[] roll(int times) {
 		int[] rolls = new int[times];
 		
-		for (int ii = 0; ii < times; ii++)
-		{
+		for (int ii = 0; ii < times; ii++) {
 			rolls[ii] = roll();
 		}
 		
@@ -50,19 +46,15 @@ public class Dice {
 	 * @param rolls An array of Rolls
 	 * @return The highest roll from this set
 	 */
-	public int max(int[] rolls)
-	{
+	public int max(int[] rolls) {
 		int max = 0;
 		
-		for (int roll : rolls)
-		{
+		for (int roll : rolls) {
 			if (roll > max) max = roll;
 		}
 		
 		return max;
 	}
-	
-
 	
 	/**
 	 * Places the rolls into an array, [0] is for 1's, [1] is for 2's etc
@@ -70,33 +62,28 @@ public class Dice {
 	 * @param sides number sided dice
 	 * @return
 	 */
-	public static int[] bin(int[] rolls, int sides)
-	{
+	public static int[] bin(int[] rolls, int sides) {
 		int[] bins = new int[sides];
 		
-		for (int roll : rolls)
-		{
+		for (int roll : rolls) {
 			bins[roll-1]++;
 		}
 		
 		return bins;
 	}
 	
-	public int[] bin(int[] rolls)
-	{
+	public int[] bin(int[] rolls) {
 		return bin(rolls, sides);
 	}
 	
 	
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		int[] arr = {1, 2, 2, 3, 4, 4, 4};
 		
 		int[] ordered = order(arr, 4);
 		int[] bin = bin(arr, 4);
 		
-		for (int i : ordered)
-		{
+		for (int i : ordered) {
 			System.out.print(i + ", ");
 		}
 		
@@ -109,17 +96,14 @@ public class Dice {
 	 * @param sides the number of sides for this dice rollset
 	 * @return a descending order dice set
 	 */
-	public static int[] order(int[] rolls, int sides)
-	{
+	public static int[] order(int[] rolls, int sides) {
 		int[] bins = bin(rolls, sides);
 		int[] ordered = new int[rolls.length];
 		
 		int index = 0;
 		
-		for (int ii = bins.length - 1 ; ii >= 0; ii--)
-		{
-			while (bins[ii] > 0)
-			{
+		for (int ii = bins.length - 1 ; ii >= 0; ii--) {
+			while (bins[ii] > 0) {
 				ordered[index] = (ii+1);
 				bins[ii]--;
 				index++;
@@ -129,8 +113,7 @@ public class Dice {
 		return ordered;
 	}
 	
-	public int[] order(int[] rolls)
-	{
+	public int[] order(int[] rolls) {
 		return order(rolls, sides);
 	}
 	
