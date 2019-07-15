de66a4f49b4e2d0eb038f767e77f88be9664a88e
jeffb <jeffb@LAPTOP-V25UAM3I.modem>
2019-07-15 11:37:06 +0800

    .

--- src/strategybots/launch/Main.java
+++ src/strategybots/launch/Main.java
@@ -1,8 +1,5 @@
 package strategybots.launch;
 
-import strategybots.bots.C4_Dummy;
-import strategybots.bots.C4_MCTS;
-import strategybots.bots.C4_Tiptaco;
 import strategybots.games.Amazons;
 import strategybots.games.Chess;
 import strategybots.games.Chomp;
@@ -37,7 +34,7 @@ public class Main {
     private static final int REVERSI = 9;
     private static final int TICTACTOE = 10;
 	
-    private static final int GAME = CONNECTFOUR;
+    private static final int GAME = TICTACTOE;
     
 	public static void main(String[] args) {
 	    
@@ -48,7 +45,7 @@ public class Main {
             case CHESS: new Chess(new ChessController(), new ChessController()); break;
 	        case CHOMP: new Chomp(new ChompController(), new ChompController()); break;
 	        case CLOBBER: new Clobber(new ClobberController(), new ClobberController()); break;
-	        case CONNECTFOUR: new ConnectFour(new ConnectFourController(), new C4_Dummy()); break;
+	        case CONNECTFOUR: new ConnectFour(new ConnectFourController(), new ConnectFourController()); break;
             case DOTSANDBOXES: new DotsAndBoxes(new DotsController(), new DotsController()); break;
             case PENTAGO: new Pentago(new PentagoController(), new PentagoController()); break;
 	        case REVERSI: new Reversi(new ReversiController(), new ReversiController()); break;
